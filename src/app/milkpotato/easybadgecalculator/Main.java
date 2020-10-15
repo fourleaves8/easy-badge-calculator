4db5735465f6f568365d592b9302213b2ef52a57
fourleaves8 <fourleaves8@gmail.com>
2020-10-15 20:35:25 +0900

    획득 가능한 총 베지수 계산

--- src/app/milkpotato/easybadgecalculator/Main.java
+++ src/app/milkpotato/easybadgecalculator/Main.java
@@ -4,21 +4,33 @@ import java.util.Scanner;
 
 public class Main {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 
 		while (true) {
-
 			Scanner scanner = new Scanner(System.in);
-			System.out.printf("최종 클리어 스테이지 : ");
+			System.out.printf("클리어한 최종 스테이지 : ");
 			int clrStage = scanner.nextInt();
 			int[] stage = new int[clrStage];
 
-			for (clrStage = 0; clrStage < stage.length; clrStage++) {
-				stage[clrStage] = clrStage + 1;
-								
-				int getBadge = stage[clrStage];
-				System.out.printf("클리어한 스테이지: %d / 획득 베지 수 : %d%n", stage[clrStage], getBadge);
+			if (clrStage > 100 || clrStage < 0) {
+				System.out.println("입력값을 다시 확인해주세요.");
+
+			} else if (clrStage == 0) {
+				System.out.println("획득한 보상이 없습니다.");
+			
+			} else {
+				int sumBadge = 0;
+				for (clrStage = 0; clrStage < stage.length; clrStage++) {
+					stage[clrStage] = clrStage + 1;
+
+					int getBadge = stage[clrStage];
+					System.out.printf("스테이지 클리어 : %d / 획득 베지 : %d%n", stage[clrStage], getBadge);
+					sumBadge = sumBadge + getBadge;
+				}
+				System.out.println("획득 가능한 총 베지 수 : " + sumBadge);
 			}
+
 		}
 
 	}
